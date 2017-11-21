a0d81154c2256b17c223d63c6df08a91b856fd59
Jaron Mink <jaronmink@gmail.com>
2017-11-21 14:31:02 -0800

    Error checking for file sys format

--- lab3a.c
+++ lab3a.c
@@ -54,18 +54,19 @@ int main(int argc, char *argv[]){
   if(imgFile[length - 1] != 'g' ||
      imgFile[length - 2] != 'm' ||
      imgFile[length - 3] != 'i' ||
-     imgFile[length - 4] != '.'){
-    
+     imgFile[length - 4] != '.'){    
     errorExitOne("File input is not of type *.img");
   }
   
-  imgfd = open(imgFile, O_RDONLY);
-
-  if(imgfd == -1){
+  if((imgfd = open(imgFile, O_RDONLY))==-1){
     errorExitOne("Error opening image file descriptor!");
   }
-  
+
   pread(imgfd, &super, sizeof(struct ext2_super_block), EXT2_MIN_BLOCK_SIZE);
+  if(super.s_magic != EXT2_SUPER_MAGIC){
+    errorExitOne("Error, file system is not in EXT2 format");
+  }
+    
   superblockSummary();
 
   return 0;
