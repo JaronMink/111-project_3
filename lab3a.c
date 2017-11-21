e6079ba976cc04d7f3009f16149731a6de2011c0
NaThong <nathongknight@yahoo.com>
2017-11-21 14:19:13 -0800

    commit

--- lab3a.c
+++ lab3a.c
@@ -12,12 +12,33 @@
 #include <string.h>
 #include "ext2_fs.h"
 
-int imgfd;
+int                     imgfd;
+struct ext2_super_block super;
+
+void errorExitNum(char* msg, int exitCode)
+{
+  fprintf(stdrr, "%s\n", msg);
+  exit(exitCode);
+}
+
+void errorExitOne(char* msg)
+{
+  errorExitNum(msg, 1);
+}
 
 void superblockSummary(){
-  dprintf(1, "SUPERBLOCK,");
+  //blocks, inodes, blocksize, inodesize, blockspergroup,inodespergroup,firstnonreservedinode
+  long numBlocks = super.s_blocks_count;
+  long blockSize= 1024 << super.s_log_block_size;
+  long numInodes = super.inodes_count;
+  long inodeSize = super.s_inode_size;
+  long blocksPerGroup = super.s_blocks_per_group;
+  long inodesPerGroup = super.s_inodes_per_group;
+  long firstFreeInode = 
+  dprintf(1, "%s,%d,%d,%d,%d,%d,%d,%d\n", "SUPERBLOCK",
+	  super.s_blocks_count, super.inodes_count,);
 
-  
+
 }
 
 int main(int argc, char *argv[]){
@@ -35,17 +56,16 @@ int main(int argc, char *argv[]){
      imgFile[length - 3] != 'i' ||
      imgFile[length - 4] != '.'){
     
-    fprintf(stderr, "File input is not of type *.img\n");
-    exit(1);
+    errorExitOne("File input is not of type *.img");
   }
   
   imgfd = open(imgFile, O_RDONLY);
 
   if(imgfd == -1){
-    fprintf(stderr, "Error opening image file descriptor!\n");
-    exit(1);
+    errorExitOne("Error opening image file descriptor!");
   }
-
+  
+  pread(imgfd, &super, sizeof(struct ext2_super_block), EXT2_MIN_BLOCK_SIZE);
   superblockSummary();
 
   return 0;
