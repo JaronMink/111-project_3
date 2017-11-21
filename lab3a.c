43e40ceb2d6e6343aea4cf6358056afd95c8c105
NaThong <nathongknight@yahoo.com>
2017-11-21 12:09:10 -0800

    working start

--- lab3a.c
+++ lab3a.c
@@ -15,21 +15,30 @@
 int imgfd;
 
 int main(int argc, char *argv[]){
-
+  
   if(argc != 2){
     fprintf(stderr, "Provide image as argument as follows: ./lab3a *.img\n");
     exit(1);
   }
-  
-  char *imgFile = argv[1];
 
+  char *imgFile = argv[1];
+  int  length   = (int)strlen(imgFile);
+  
+  if(imgFile[length - 1] != 'g' ||
+     imgFile[length - 2] != 'm' ||
+     imgFile[length - 3] != 'i' ||
+     imgFile[length - 4] != '.'){
+    
+    fprintf(stderr, "File input is not of type *.img\n");
+    exit(1);
+  }
+  
   imgfd = open(imgFile, O_RDONLY);
 
   if(imgfd == -1){
     fprintf(stderr, "Error opening image file descriptor!\n");
     exit(1);
   }
-  
-  printf("%s\n", imgFile);
+
   return 0;
 }
