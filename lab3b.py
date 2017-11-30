d071cb552274fa21ccc1c8c7f40b4349b3ffac95
jmink <jmink@novacoast.com>
2017-11-29 22:23:57 -0800

    began creating tests in blockAudit

--- lab3b.py
+++ lab3b.py
@@ -1,15 +1,128 @@
 #!/usr/local/cs//bin/python3
 
 import sys #to get arguements
-from enum import IntEnum, auto
+#from enum import IntEnum, auto
+no_inconsistencies_found = 0
+superBlock = None
+groups = []
+inodes = []
+freeBlocks = []
+freeInodes = []
+directories = []
+indirects = []
 
-#no_inconsistencies_found = True
+class SuperBlock:
+    '''
+    def __init__(self, csv_file):  # //totalBlocks, totalInodes, firstNonReservedInode):
+        firstLine = csv_file.readline()
+
+    '''
+    def __init__(self, csv_line):
+        parsedLine = csv_line.split(',')
+        self.name = parsedLine[0]
+        self.totalBlocks = int(parsedLine[1])
+        self.totalInodes = int(parsedLine[2])
+        self.blockSize = int(parsedLine[3])
+        self.inodeSize = int(parsedLine[4])
+        self.blocksPerGroup = int(parsedLine[5])
+        self.inodesPerGroup = int(parsedLine[6])
+        self.firstNonReservedInode = int(parsedLine[7])
+class Group:
+    def __init__(self, csv_line):
+        parsedLine = csv_line.split(',')
+        self.name = parsedLine[0]
+        self.groupNum = int(parsedLine[1])
+        self.totalBlocks = int(parsedLine[2])
+        self.totalInodes = int(parsedLine[3])
+        self.numFreeBlocks = int(parsedLine[4])
+        self.numFreeInodes = int(parsedLine[5])
+        self.blockBitmap = int(parsedLine[6])
+        self.inodeBitmap = int(parsedLine[7])
+        self.firstNonReservedInode = int(parsedLine[8])
+        #self.firstUnreservedBlock = self.firstUnreservedInode
+
+class FreeBlock:
+    def __init__(self,csv_line):
+        parsedLine = csv_line.split(',')
+        self.name = parsedLine[0]
+        self.blockNum= int(parsedLine[1])
+
+class FreeInode:
+    def __init__(self,csv_line):
+        parsedLine = csv_line.split(',')
+        self.name = parsedLine[0]
+        self.blockNum= int(parsedLine[1])
+
+class Inode:
+    def __init__(self, csv_line):
+        parsedLine = csv_line.split(',')
+        self.name = parsedLine[0]
+        self.inodeNum = int(parsedLine[1])
+        self.fileType = parsedLine[2]
+        self.mode = parsedLine[3]
+        self.owner = parsedLine[4]
+        self.group = parsedLine[5]
+        self.linkCount = int(parsedLine[6])
+        self.lastInodeChange = parsedLine[7]
+        self.timeOfLastModification = parsedLine[8]
+        self.timeOfLastAccess = parsedLine[9]
+        self.fileSize = int(parsedLine[10])
+        self.numBlocks = int(parsedLine[11])
+        self.i_blocks = parsedLine[12:]
+
+class Directory:
+    def __init__(self,csv_line):
+        parsedLine = csv_line.split(',')
+        self.name = parsedLine[0]
+        self.parentInodeNum= int(parsedLine[1])
+        self.logicalByteOffset= int(parsedLine[2])
+        self.inodeNum= int(parsedLine[3])
+        self.entryLen= int(parsedLine[4])
+        self.nameLen= int(parsedLine[5])
+        self.name= parsedLine[6][:-1]
+        #print(self.name, end="")
+
+class Direct:
+    def __init__(self, blockNum, inodeNum):
+        self.name = 'DIRECT'
+        self.blockNum = blockNum
+        self.inodeNum = inodeNum
+        self.logicalOffset = 0
+
+class Indirect:
+    def __init__(self, csv_line):
+        parsedLine = csv_line.split(',')
+        self.name = parsedLine[0]
+        self.owningInodeNum = int(parsedLine[1])
+        self.indirectionLevel = int(parsedLine[2])
+        self.logicalOffset = int(parsedLine[3])
+        self.indirectBlockNum = int(parsedLine[4])
+        self.blockNum = int(parsedLine[5])
+'''
+class Block:
+    def __init__(self, blockType, blockNum, blockLevel, offset, inodeNum):
+        self.blockType = blockType
+        self.blockNum = blockNum
+        self.blockLevel = blockLevel
+        self.offset = offset
+        self.inodeNum = inodeNum
+        '''
+'''class Spot(IntEnum):
+    FREE = auto()
+    RESERVED = auto()
+    TAKEN = auto()
+'''
+#define numeric constants to use, values do not matter
+FREE = 1
+RESERVED = 2
+TAKEN = 3
 
 def errorExitNum(msg, num):
     print(msg)
     exit(num)
 def errorExitOne(msg):
     errorExitNum(msg, 1)
+
 
 def getCSVFile():
     if(len(sys.argv) < 2):
@@ -23,157 +136,147 @@ def getCSVFile():
 
     return open(csv_file_str, 'r') #open csv file in read only
 
-#need to know why it is being used Free, Reserved, File
-#one for each block/indone
-#class Spot:
- #   type = "" #File, Inode, ReservedFile, Free
-
-def blockFromCSVLine(line):
+def addObjectFromCSVLine(line):
+    global no_inconsistencies_found
     global superBlock
+    global groups
+    global inodes
+    global freeBlocks
+    global freeInodes
+    global directories
+    global indirects
+
     parseLine = line.split(',')
     if parseLine[0] == 'SUPERBLOCK':
-        return superBlock
+        object = SuperBlock(line)
+        superBlock = object
     elif parseLine[0] == 'GROUP':
-        #first unreserved block is the block of the i-nodes, plus the how many blocks the inodeTable is
-        firstUnreservedBlockInGroup = int(parseLine[8]) + (superBlock.inodeSize*superBlock.totalInodes)/superBlock.blockSize 
-        return Group(int(parseLine[4]), int(parseLine[5]), firstUnreservedBlockInGroup)
-    elif parseLine[0] == 'BFREE' || parseLine[0] == 'INDIRECT' || 
-        return Block(
+        object = Group(line)
+        groups.append(object)
+    elif parseLine[0] == 'BFREE':
+        object = FreeBlock(line)
+        freeBlocks.append(object)
+    elif parseLine[0] == 'IFREE':
+        object = FreeInode(line)
+        freeInodes.append(object)
+    elif parseLine[0] == 'INODE':
+        object = Inode(line)
+        inodes.append(object)
+    elif parseLine[0] == 'DIRENT':
+        object = Directory(line)
+        directories.append(object)
+    elif parseLine[0] == 'INDIRECT':
+        object = Indirect(line)
+        indirects.append(object)
+    else:
+        errorExitOne("Error, csv file not formatted properly")
 
-class Block:
-    #blockType  #Free, Reserved, Taken
-    #blockNum
-    #blockLevel
-    #offset
-    #inodeNum
-
-    def __init__(self, blockType, blockNum, blockLevel, offset, inodeNum):
-        self.blockType = blockType
-        self.blockNum = blockNum
-        self.blockLevel = blockLevel
-        self.offset = offset
-        self.inodeNum = inodeNum
-
-class Spot(IntEnum):
-    FREE = auto()
-    RESERVED = auto()
-    TAKEN = auto()
-
-    
-class SuperBlock:
-    #totalBlocks
-    #totalInodes
-    #firstNonReservedInode
-
-    def __init__(self, csv_file): #//totalBlocks, totalInodes, firstNonReservedInode):
-        firstLine = csv_file.readline()
-        parsedLine = firstLine.split(',')
-        self.totalBlocks = int(parsedLine[1])
-        self.totalInodes = int(parsedLine[2])
-        self.blockSize = int(parsedLine[3])
-        self.inodeSize = int(parsedLine[4])
-        self.firstNonReservedInode = int(parsedLine[7])
-
-class Group:
-    def __init__(self, numBlocks, numInodes, firstUnreservedBlock):
-        self.numBlocks = numBlocks
-        self.numInodes = numInodes
-        self.firstUnreservedBlock = firstUnreservedBlock
+def initializeDataFromCSV(csv_file):
+    for line in csv_file:
+        addObjectFromCSVLine(line)
 
 #returns true if block is valid
-def checkBlockValidity(block, firstUnreservedBlock, lastBlock):
+def checkBlockValidity(block, lastBlock):
     if block.blockNum < 0 or block.blockNum > lastBlock:
-        print('error, invalid block')
+        if(block.name == 'DIRECT'):
+            name = 'BLOCK'
+
+        print('INVALID %s %d IN INODE %d AT OFFSET %d' % (name, block.blockNum, block.inodeNum, block.logicalOffset))
         return False
-    if block.blockNum < firstUnreservedBlock :
-        print('Error, reserved block')
-        return False
+    #if block.blockNum < firstUnreservedBlock :
+     #   print('Error, reserved block')
+      #  return False
     return True
 
-def addAllBlocks(blocks, csv_file):
+def checkIfFreeOrReserved(blocks, block):
+    if blocks[block.blockNum] == FREE:
+        print('Error, allocated Free Block')
+        return True
+    elif blocks[block.blockNum] == RESERVED:
+        print('Error, block is reserved')
+        return False
+    else:
+        return True
+
+def addAllBlocks(blockList):
+    global FREE
+    global RESERVED
+    global no_inconsistencies_found
     global superBlock
-    for line in csv_file:
-        block =  blockFromCSVLine(line)
-        if block.isclass(SuperBlock):
-            print('Super')
-            continue
-        elif block.isclass(Group): #if group, set reserved blocks in list
-            firstUnreservedBlock = block.firstUnreservedBlock
-            blocks[:firstUnreservedBlock - 1] = [Spot.RESERVED]*firstUnreservedBlock
-        elif block.isclass(Block) == True:
-            if block.blockType == 'INDIRECT':
-                if checkBlockValidity(block, firstUnreservedBlock, superBlock.totalBlocks - 1):
-                    print('Indirect block added at %d', block.blockNum)
-                    blocks[block.blockNum] = blocks[block.blockNum].append(blocks)
-            if block.blockType == 'BFREE':
+    global groups
+    global inodes
+    global freeBlocks
+    global freeInodes
+    global directories
+    global indirects
+    #add reserved blocks
+    #since only 1 block, we can find all restricted blocks through first group
+    firstUnreservedBlock = groups[0].firstNonReservedInode + (superBlock.inodeSize * superBlock.totalInodes/superBlock.blockSize)
+    blockList[:firstUnreservedBlock] = firstUnreservedBlock*[RESERVED]
 
-            if block.blockType == 'IFREE':
-                print('Ifree')
+    #addFreeBlocks
+    for freeBlock in freeBlocks:
+        if(checkBlockValidity(freeBlock, superBlock.totalBlocks - 1)):
+            blockList[freeBlock.blockNum] = [FREE]
 
-            if block.blockType == 'DIRENT':
-                print('Dirent')
+    #add indirectBlocks
+    for indirectBlock in indirects:
+        if(checkBlockValidity(indirectBlock, superBlock.totalBlocks - 1) and checkIfFreeOrReserved(blockList, indirectBlock)):
+            blockList[indirectBlock.blockNum] = blockList[indirectBlock.blockNum].append(indirectBlock)
 
-            if block.blockType == 'INODE':
-                print('Inode')
+    #for block in
+    for inode in inodes:
+        for i in range(0, 12): #may need to include indirect pointers???? see later
+            if int(inode.i_blocks[i]) == 0:
+                continue
+            directBlock = Direct(int(inode.i_blocks[i]), inode.inodeNum)
+            if(checkBlockValidity(directBlock, superBlock.totalBlocks -1) and checkIfFreeOrReserved(blockList, directBlock)):
+                blockList[directBlock.blockNum] = blockList[directBlock.blockNum].append(directBlock)
+    #add
 
-        
+    #for
+
 #reports if
 #  any block is <0 or > highest block
 #  any block takes a reserved block space
 #  any block is not referenced by a freelist and not referenced by a file
 #  any block is refereneced by a freelist and a file
 #  any block is referenced by multiple files
-def blockConsistencyAudit(csv_file):
+def blockConsistencyAudit():
     global no_inconsistencies_found
     global superBlock
 
     #create list stucture for all blocks
     #add reserved list to structure
     #add free lists to structure
-
     #create list of empty lists, one lise for each open block
     blocks = [[] for i in range (superBlock.totalBlocks)]
+    addAllBlocks(blocks)
 
-    addAllBlocks(blocks, csv_file)
-    
-    #create structure to store all block info,
-    #for line in csv_file:
-        #checks if valid
-        #try to add to list, if space is already taken(non-null), error must be reported
-        
-        
-        #checkBlockValidity
-        #checkBlockReservations
-        #check if already referenced by another file
-        #
-        #pri
 
-    #check if any blocks were not referenced in block list at all
 def inodeAllocationAudit(csv_file):
     global no_inconsistencies_found
-    
+
 
 def directoryConsistencyAudit(csv_file):
     global no_inconsistencies_found
 
 
 def main():
-    global no_inconsistencies_found
-    global superBlock
-    
+
+
     no_inconsistencies_found = 0
-    #first get open input file    
+    # first get open input file
     csv_file = getCSVFile()
 
-    #totalBlocks, totalInodes, first nonreservedInode
-    superBlock = SuperBlock(csv_file)
-        
-    blockConsistencyAudit(csv_file)
-            
+    initializeDataFromCSV(csv_file)
+
+    # totalBlocks, totalInodes, first nonreservedInode
+    blockConsistencyAudit()
+
     csv_file.close()
     exit(no_inconsistencies_found)
 
-    
-if __name__ == '__main__': #run main function
-    main()
 
+if __name__ == '__main__':  # run main function
+    main()
